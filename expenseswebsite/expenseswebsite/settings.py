da9d1d494861b8443dc53a77aad26a6eb1669e45
Utkarsh Mandal <utkarshmandal420@gmail.com>
2024-08-02 17:39:13 +0530

    Deploying

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-wgf^=(t!qmv3wxwr973489p79e^8tu_wz$c90vt9)#9ibl*pg#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
